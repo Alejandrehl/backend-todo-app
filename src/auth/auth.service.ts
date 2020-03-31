276b188d4b5e7ec48dee7eb9f3c130d53c58313e
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-31 16:58:07 -0300

    login method and validate user service done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as jwtDecode from 'jwt-decode';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
+  async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(email);
-    if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
+    const match = await bcrypt.compare(pass, user.password);
+
+    if (!match) return null;
+
+    const { password, isActive, ...result } = user;
+    return this.jwtService.sign(result);
   }
 }
