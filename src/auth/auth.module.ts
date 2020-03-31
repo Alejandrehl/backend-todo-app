e037549c8b05bc13f65ef1092487622c7b092a79
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-31 13:58:03 -0300

    Compilando sin errores

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
+import { UsersModule } from './../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
