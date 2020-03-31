d1521f055dc1fbfa72b066b406446d6e2d90e1a6
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-31 17:35:16 -0300

    se aumento la duracion del token

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -13,7 +13,7 @@ import { jwtConstants } from '../auth/constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '360000s' },
     }),
   ],
   providers: [UsersService],
