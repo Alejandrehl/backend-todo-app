b158f562805b56a7309faaf799655d1137aabe7a
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-31 19:44:48 -0300

    save

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,3 +1,4 @@
+import { UsersModule } from './../users/users.module';
 import { Task } from './task.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
@@ -5,7 +6,7 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), UsersModule],
   providers: [TasksService],
   controllers: [TasksController],
 })
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task)
     private taskRepository: Repository<Task>,
+    private usersService: UsersService,
   ) {}
 
   async create(newTask: CreateTaskDto): Promise<Task> {
