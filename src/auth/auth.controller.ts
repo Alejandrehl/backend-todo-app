d397c8d34246599a88236a08d721c097a0c443ab
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-03-31 14:14:13 -0300

    save

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  Post,
+  HttpStatus,
+  UnauthorizedException,
+  Req,
+  Res,
+} from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Req() request, @Res() response) {
+    const { email, password } = request.body;
+
+    this.authService
+      .validateUser(email, password)
+      .then(user => {
+        if (!user) throw new UnauthorizedException();
+        return response.status(HttpStatus.OK).json(user);
+      })
+      .catch(err => response.status(HttpStatus.FORBIDDEN).json(err));
+  }
+}
